7071824ff629d3f6cfe34c85f2a9ef72543c26f3
Md Faisal Sohail <faisalkv1963@gmail.com>
2021-03-10 23:59:03 +0530

    generalise business Logic

--- MySketch/src/main/java/RollingBall.java
+++ MySketch/src/main/java/RollingBall.java
@@ -1,14 +1,42 @@
-public class RollingBall {
-     int speedAlongX;
-     float distanceFromTop;
-     int positionOnXaxis=0;
-    public static final int Diameter = 10;
-    /*public RollingBall(){
-        speedAlongX=0;
-        distanceFromTop=0;
-    }*/
-    public RollingBall(int speedAlongX, float distanceFromTop) {
-        this.speedAlongX = speedAlongX;
-        this.distanceFromTop = distanceFromTop;
-    }
+
+import processing.core.PApplet;
+
+interface Shape{
+    public void move();
+    public void draw(PApplet PApplet);
 }
+
+
+class Ball implements Shape {
+    int xCoordiante;
+    int yCoordinate;
+    int speedAlongX;
+    int diameter;
+    Ball(int xCoordiante,int yCoordinate,int speedAlongX,int diameter){
+        this.xCoordiante=xCoordiante;
+        this.yCoordinate=yCoordinate;
+        this.speedAlongX=speedAlongX;
+        this.diameter=diameter;
+    }
+
+    @Override
+    public void move() {
+        xCoordiante+=speedAlongX;
+
+    }
+
+    @Override
+    public void draw(PApplet PApplet) {
+        drawCircle(PApplet);
+    }
+
+    private void drawCircle(PApplet PApplet) {
+        PApplet.ellipse(xCoordiante, yCoordinate, diameter, diameter);
+    }
+
+
+}
+
+
+
+
--- MySketch/src/main/java/Sketch.java
+++ MySketch/src/main/java/Sketch.java
@@ -1,44 +1,40 @@
 import processing.core.PApplet;
+import java.util.ArrayList;
 
 public class Sketch extends PApplet{
-
     public static final int WIDTH = 680;
     public static final int HEIGHT = 480;
-    public static final int TOTAL_BALLS=4;
-    RollingBall Balls[]=new RollingBall[TOTAL_BALLS];
+    ArrayList<Shape> shapes;
+
+    {
+        shapes = new ArrayList<Shape>();
+    }
 
     public static void main(String[] args) {
+
         PApplet.main("Sketch",args);
     }
 
     @Override
     public void settings() {
+
         size(WIDTH, HEIGHT);
     }
 
     @Override
     public void setup() {
-        createBalls();
+
+        shapes.add(new Ball(0,HEIGHT/4,1,10));
+        shapes.add(new Ball(0,(HEIGHT*2)/4,2,10));
+        shapes.add(new Ball(0,(HEIGHT*3)/4,3,10));
     }
 
     @Override
     public void draw() {
-        for(int ball=1;ball<=TOTAL_BALLS;ball++){
-            drawCircle(Balls[ball - 1]);
-            Balls[ball-1].positionOnXaxis+=Balls[ball-1].speedAlongX;
+        shapes.forEach(Shape::move);
+        for (Shape shape : shapes) {
+            shape.draw(this);
         }
 
-    }
-    private void createBalls() {
-
-        for(int ball=1;ball<=TOTAL_BALLS;ball++){
-            int speedOfBall=ball;
-            float distanceFromTop=(HEIGHT*ball)/5;
-            Balls[ball-1]=new RollingBall(speedOfBall,distanceFromTop);
-        }
-    }
-
-    private void drawCircle(RollingBall ball) {
-        ellipse(ball.positionOnXaxis, ball.distanceFromTop, ball.Diameter,ball.Diameter);
     }
 }
